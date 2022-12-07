1f35e0ac9c3b5831731f50f5f4bcf0eb60f41379
Samuel Junior dev <116283407+samuellima22@users.noreply.github.com>
2022-12-07 20:11:13 -0300

    Create main.java

---
+++ Classes-JAVA/Forma/main.java
@@ -0,0 +1,13 @@
+from biblioteca import Forma
+from biblioteca import Retangulo
+from biblioteca import Triangulo
+
+f1 = Forma('','')
+t1 = Triangulo(10,10)
+r1 = Retangulo(10,10)
+
+r1.calcularArea_r(10,10)
+r1.calcularPerimetro_r(10,10)
+
+t1.calcularArea_t(10,10)
+t1.calcularPerimetro_t(10,10,5)
