f7b5084afca96c15fbd7b65766c98666961e06eb
Samuel Junior dev <116283407+samuellima22@users.noreply.github.com>
2022-12-07 19:30:09 -0300

    Create StringExercicio06.java

---
+++ Strings-JAVA/StringExercicio06.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class StringExercicio06 {
+	public static void main(String[] args) {
+String[] vetor = {"a","vida","é","bela"};
+		
+		String textao = vetor[0] +" "+ vetor[1]+" " + vetor[2] +" "+ vetor[3];
+		char[] b = textao.to();
+		System.out.println(textao.toCharArray());
+			
+		}
+	}
