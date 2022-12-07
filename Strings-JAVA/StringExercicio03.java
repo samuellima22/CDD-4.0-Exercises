55818168c65f12de77c2963f113f1d461368580a
Samuel Junior dev <116283407+samuellima22@users.noreply.github.com>
2022-12-07 19:28:24 -0300

    Create StringExercicio03.java

---
+++ Strings-JAVA/StringExercicio03.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class StringExercicio03 {
+	public static void main(String[] args) {
+		String texto01 = "Será que são iguais?";
+		String texto02 = "Será que são iguais?";
+		
+		if (texto01.equals(texto02)) {
+			System.out.println("São iguais");
+		}
+		else {
+			System.out.println("Não são iguais");
+		}
+	}
+}
