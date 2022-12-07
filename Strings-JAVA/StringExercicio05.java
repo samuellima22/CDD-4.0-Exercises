348e0f38154bfacc31a29440235f9bfa932c99ec
Samuel Junior dev <116283407+samuellima22@users.noreply.github.com>
2022-12-07 19:29:40 -0300

    Create StringExercicio05.java

---
+++ Strings-JAVA/StringExercicio05.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class StringExercicio05 {
+
+	public static void main(String[] args) {
+		String[] vetor = {"a","vida","é","bela"};
+		
+		String textao = vetor[0] +" "+ vetor[1]+" " + vetor[2] +" "+ vetor[3];
+		System.out.println(textao.toUpperCase());
+		
+	}
+
+}
