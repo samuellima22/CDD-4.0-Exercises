1bff2abdfe2d264abd04fe0d8c226145e1129b5a
Samuel Junior dev <116283407+samuellima22@users.noreply.github.com>
2022-12-07 19:26:30 -0300

    Create StringExercicio01.java

---
+++ Strings-JAVA/StringExercicio01.java
@@ -0,0 +1,9 @@
+package fundamentos;
+
+public class StringExercicio01 {
+	public static void main(String[] args) {
+		String str = " texto para retirar espaços no início e fim ";
+		String resultado = str.trim();
+		System.out.println(resultado);
+	}
+}
